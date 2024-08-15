a6919d28a46366795230900677d0b476af01bcb2
Gagan <sandhugagandeep0601@gmail.com>
2024-08-15 12:57:18 -0400

    second

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 // PostgreSQL connection pool
 const pool = new Pool({
-    user: 'postgres', // replace with your PostgreSQL username
-    host: 'localhost',
-    database: 'translations', // replace with your database name
-    password: "Gagan@0601", // replace with your PostgreSQL password
+    user: 'translations_cohy_user', // replace with your PostgreSQL username
+    host: 'dpg-cqv33co8fa8c73feki50-a',
+    database: 'translations_cohy', // replace with your database name
+    password: "cChOjMdHyI6obQxfmzLgXNF3cxdUdOg3", // replace with your PostgreSQL password
     port: 5432,
 });
 module.exports = pool;
