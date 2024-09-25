139105bf66be30d7725cf25852d803bf10e1effc
Gagan <sandhugagandeep0601@gmail.com>
2024-09-25 14:59:53 -0400

    little more change

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.post('/translate', async (req, res) => {
     // Save the translation result to the database
     await pool.query(
       'INSERT INTO translations (language, message, translated_text) VALUES ($1, $2, $3)',
-      [languageNames, message, translatedText]
+      [languageNames[language], message, translatedText]
     );
 
 
